9d4d34b63abfa00203d90cffba0eea8524d6068e
Hacktix <zecmad16@htlkaindorf.at>
2023-04-16 01:24:19 +0200

    implement feature enable command safeguard

---
+++ Patches/BuildInfoFeaturePatcher.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Winch.Core;
+
+namespace Winch.Patches
+{
+    [HarmonyPatch(typeof(GameManager))]
+    [HarmonyPatch("AddTerminalCommands")]
+    class BuildInfoFeaturePatcher
+    {
+        static bool Prefix() {
+            WinchCore.Log.Debug("Disallowed Feature Enable Commands to be added to Terminal.");
+            return false;
+        }
+    }
+}
