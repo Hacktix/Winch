9c780b5bbc25e25dde8cf411fca56420298bb19a
Hacktix <zecmad16@htlkaindorf.at>
2023-04-15 14:12:07 +0200

    initial commit

---
+++ Loader.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+
+namespace Winch
+{
+    public class Loader
+    {
+        public static void Main()
+        {
+            var harmony = new Harmony("com.dredge.winch");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ Patches/GameLoadHook.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System.IO;
+
+namespace Winch.Patches
+{
+    [HarmonyPatch(typeof(GameManager))]
+    [HarmonyPatch("WaitForAllAsyncManagers")]
+    class GameLoadHook
+    {
+        static void Postfix()
+        {
+            GameManager.Instance.BuildInfo.BuildNumber += "\nWinch Loaded!";
+        }
+    }
+}
