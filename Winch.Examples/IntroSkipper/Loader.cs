9d95a63076dd0a6cf7c3683b340ea2a5fc310a10
Hacktix <zecmad16@htlkaindorf.at>
2023-04-16 14:12:27 +0200

    restructure repository

---
+++ Winch.Examples/IntroSkipper/Loader.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Winch.Core;
+
+namespace IntroSkipper
+{
+    public class Loader
+    {
+        public static void Initialize()
+        {
+            WinchCore.Log.Info("Applying Harmony Patches...");
+            var harmony = new Harmony("com.dredge.introskipper");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ Winch.Examples/IntroSkipper/SplashControllerPatcher.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace IntroSkipper
+{
+    [HarmonyPatch(typeof(SplashController))]
+    [HarmonyPatch("OnEnable")]
+    class SplashControllerPatcher
+    {
+        public static bool Prefix()
+        {
+            GameManager.Instance.Loader.LoadStartupFromSplash();
+            return false;
+        }
+    }
+}
