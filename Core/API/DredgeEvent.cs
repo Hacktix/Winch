11a76382e968c68e7fb9cdfd1df72484d4fcf967
Hacktix <zecmad16@htlkaindorf.at>
2023-04-16 13:52:44 +0200

    add item load event triggers

---
+++ Core/API/DredgeEvent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Winch.Core.API
+{
+    public static class DredgeEvent
+    {
+        public class AddressablesLoadedEventArgs<T> : EventArgs
+        {
+            public AsyncOperationHandle<IList<T>> Handle;
+
+            public AddressablesLoadedEventArgs(AsyncOperationHandle<IList<T>> handle)
+            {
+                Handle = handle;
+            }
+        }
+        public delegate void AddressablesLoadedEventHandler<T>(object sender, AddressablesLoadedEventArgs<T> e);
+
+        public static event AddressablesLoadedEventHandler<ItemData> BeforeItemsLoaded;
+        public static event AddressablesLoadedEventHandler<ItemData> ItemsLoaded;
+        internal static void TriggerItemsLoaded(object sender, AsyncOperationHandle<IList<ItemData>> loadHandle, bool prefixTrigger)
+        {
+            WinchCore.Log.Debug($"Triggered ItemsLoaded type event: {loadHandle.Result.Count} elements");
+            AddressablesLoadedEventArgs<ItemData> args = new AddressablesLoadedEventArgs<ItemData>(loadHandle);
+            if(prefixTrigger)
+            {
+                WinchCore.Log.Debug($"Invoking BeforeItemsLoaded Event...");
+                BeforeItemsLoaded?.Invoke(sender, args);
+            }
+            else
+            {
+                WinchCore.Log.Debug($"Invoking ItemsLoaded Event...");
+                ItemsLoaded?.Invoke(sender, args);
+            }
+        }
+    }
+
+}
---
+++ Patches/API/ItemLoadPatcher.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using Winch.Core.API;
+
+namespace Winch.Patches.API
+{
+    [HarmonyPatch(typeof(ItemManager))]
+    [HarmonyPatch("OnItemDataAddressablesLoaded")]
+    class ItemLoadPatcher
+    {
+        public static void Prefix(ItemManager __instance, AsyncOperationHandle<IList<ItemData>> handle)
+        {
+            DredgeEvent.TriggerItemsLoaded(__instance, handle, true);
+        }
+
+        public static void Postfix(ItemManager __instance, AsyncOperationHandle<IList<ItemData>> handle)
+        {
+            DredgeEvent.TriggerItemsLoaded(__instance, handle, false);
+        }
+    }
+}
