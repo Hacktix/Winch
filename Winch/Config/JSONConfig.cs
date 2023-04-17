ab92d85bfa1ce86e0511e3d95b71d5b1ea22cade
Hacktix <zecmad16@htlkaindorf.at>
2023-04-17 19:32:27 +0200

    extract JSONConfig from WinchConfig

---
+++ Winch/Config/JSONConfig.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Winch.Config
+{
+    public class JSONConfig
+    {
+        private Dictionary<string, object> _config;
+        private string _configPath;
+
+        public JSONConfig(string path, string defaultConfig) {
+            _configPath = path;
+
+            if (File.Exists(_configPath))
+            {
+                string confText = File.ReadAllText(_configPath);
+                _config = JsonConvert.DeserializeObject<Dictionary<string, object>>(confText);
+            }
+            else
+            {
+                File.WriteAllText(_configPath, defaultConfig);
+                _config = JsonConvert.DeserializeObject<Dictionary<string, object>>(defaultConfig);
+            }
+        }
+
+        public T GetProperty<T>(string key, T defaultValue)
+        {
+            if (!_config.ContainsKey(key))
+            {
+                SetProperty(key, defaultValue);
+                return defaultValue;
+            }
+            return (T)_config[key];
+        }
+
+        public void SetProperty<T>(string key, T value)
+        {
+            _config[key] = value;
+            SaveSettings();
+        }
+
+        private void SaveSettings()
+        {
+            string confText = JsonConvert.SerializeObject(_config, Formatting.Indented);
+            File.WriteAllText(_configPath, confText);
+        }
+    }
+}
--- Winch/Config/WinchConfig.cs
+++ Winch/Config/WinchConfig.cs
@@ -1,24 +1,13 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Reflection;
 
 namespace Winch.Config
 {
-    public class WinchConfig
+    public class WinchConfig : JSONConfig
     {
-        private Dictionary<string, object> _config;
-        private static readonly string _configPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "WinchConfig.json");
+        private static readonly string WinchConfigPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "WinchConfig.json");
 
-        private WinchConfig() {
-            if(File.Exists(_configPath))
-            {
-                string confText = File.ReadAllText(_configPath);
-                _config = JsonConvert.DeserializeObject<Dictionary<string, object>>(confText);
-            }
-            else
-                _config = GenerateDefaultConfig();
-        }
+        private WinchConfig() : base(WinchConfigPath, Properties.Resources.DefaultConfig) { }
 
         private static WinchConfig _instance;
         public static WinchConfig Instance
@@ -31,42 +20,9 @@ namespace Winch.Config
             }
         }
 
-
-
-        private static Dictionary<string, object> GenerateDefaultConfig()
+        public static new T GetProperty<T>(string key, T defaultValue)
         {
-            string confText = Properties.Resources.DefaultConfig;
-            File.WriteAllText(_configPath, confText);
-            return JsonConvert.DeserializeObject<Dictionary<string, object>>(confText);
-        }
-
-        public static T GetProperty<T>(string key, T defaultValue)
-        {
-            return Instance._getProperty<T>(key, defaultValue);
-        }
-
-
-
-        private T _getProperty<T>(string key, T defaultValue)
-        {
-            if (!_config.ContainsKey(key))
-            {
-                _setProperty(key, defaultValue);
-                return defaultValue;
-            }
-            return (T)_config[key];
-        }
-
-        private void _setProperty<T>(string key, T value)
-        {
-            _config[key] = value;
-            SaveSettings();
-        }
-
-        private void SaveSettings()
-        {
-            string confText = JsonConvert.SerializeObject(_config, Formatting.Indented);
-            File.WriteAllText(_configPath, confText);
+            return ((JSONConfig)Instance).GetProperty(key, defaultValue);
         }
     }
 }
