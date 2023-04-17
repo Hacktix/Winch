9952140231a1244f2cb560b97b4f81549ba2985d
Hacktix <zecmad16@htlkaindorf.at>
2023-04-17 18:23:06 +0200

    add DisasterButton example mod

---
+++ Winch.Examples/DisasterButton/DisasterButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace DisasterButton
+{
+    class DisasterButton : MonoBehaviour
+    {
+        private static System.Random rnd = new System.Random();
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Delete))
+                OnPress();
+        }
+
+        private void OnPress()
+        {
+            if (GameManager.Instance == null || GameManager.Instance.DataLoader == null || GameManager.Instance.WorldEventManager == null)
+                return;
+
+            WinchCore.Log.Debug("DisasterButton initialized.");
+            int index = rnd.Next(GameManager.Instance.DataLoader.allWorldEvents.Count);
+            WorldEventData worldEvent = GameManager.Instance.DataLoader.allWorldEvents[index];
+            WinchCore.Log.Debug($"Spawning event No. {index}: {worldEvent.name}");
+            GameManager.Instance.WorldEventManager.DoEvent(worldEvent);
+        }
+    }
+}
---
+++ Winch.Examples/DisasterButton/Loader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Winch.Core.API;
+
+namespace DisasterButton
+{
+    public class Loader
+    {
+        public static void Initialize()
+        {
+            DredgeEvent.ManagersLoaded += InitializeDisasterButton;
+        }
+
+        private static void InitializeDisasterButton(object sender, System.EventArgs e)
+        {
+            GameObject gameObject = new GameObject();
+            gameObject.AddComponent<DisasterButton>();
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+}
