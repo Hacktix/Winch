fb30421aac2707e1b7b5cf049c273fb3ff6907f3
Hacktix <zecmad16@htlkaindorf.at>
2023-04-22 23:12:35 +0200

    add automated item asset loading

--- Winch/Core/AssetLoader.cs
+++ Winch/Core/AssetLoader.cs
@@ -24,13 +24,38 @@ namespace Winch.Core
         {
             string localizationFolderPath = Path.Combine(path, "Localization");
             string textureFolderPath = Path.Combine(path, "Textures");
+            string itemFolderPath = Path.Combine(path, "Items");
 
             if(Directory.Exists(localizationFolderPath)) LoadLocalizationFiles(localizationFolderPath);
             if(Directory.Exists(textureFolderPath)) LoadTextureFiles(textureFolderPath);
+            if(Directory.Exists(itemFolderPath)) LoadItemFiles(itemFolderPath);
         }
 
 
 
+        private static void LoadItemFiles(string itemFolderPath)
+        {
+            string spatialItemsPath = Path.Combine(itemFolderPath, "General");
+
+            if(Directory.Exists(spatialItemsPath)) LoadItemFilesOfType<SpatialItemData>(spatialItemsPath);
+        }
+
+        private static void LoadItemFilesOfType<T>(string itemFolderPath) where T : ItemData
+        {
+            string[] itemFiles = Directory.GetFiles(itemFolderPath);
+            foreach(string file in itemFiles)
+            {
+                try
+                {
+                    ItemUtil.AddItemFromMeta<T>(file);
+                }
+                catch(Exception ex)
+                {
+                    WinchCore.Log.Error($"Failed to load item from {file}: {ex}");
+                }
+            }
+        }
+        
         private static void LoadLocalizationFiles(string localizationFolderPath)
         {
             string[] localizationFiles = Directory.GetFiles(localizationFolderPath);
