a0b347ed34b555f85cb2d63e4cec966c1ec9e3b2
bdlm-dev <74147859+bdlm-dev@users.noreply.github.com>
2023-04-28 15:15:36 +0100

    Update check (#12)
    * Ignore unrelated files
    
    * Remove unnecessary solution
    
    * Add check for newer release
    
    * Revert "Remove unnecessary solution"
    
    This reverts commit ff2f900d81f6fc6150427fc98e1b63aafe9d10fc.
    
    * Revert "Ignore unrelated files"
    
    This reverts commit d59d65f2f2116e57c68819f1a92aa391b9a0e164.
    
    * Revert to original
    
    * Prevent gitignore pushing itself
    
    * Switch to system HttpClient request method.
    
    * Update gitignore
    
    * Update .gitignore
    
    * Update Winch.csproj
    
    * Update gitignore
    
    * Update .gitignore

--- Winch/Core/Initializer.cs
+++ Winch/Core/Initializer.cs
@@ -1,8 +1,11 @@
 ﻿using CommandTerminal;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using Winch.Config;
 using Winch.Core.API;
 using Winch.Util;
+using static System.Net.WebRequestMethods;
 
 namespace Winch.Core
 {
@@ -23,13 +26,16 @@ namespace Winch.Core
         internal static void InitializePostUnityLoad()
         {
             InitializeVersionLabel();
+
+            if (WinchConfig.GetProperty("CheckForUpdates", true))
+                CheckForUpdate();
         }
 
         private static void InitializeAssetLoader()
         {
             GameObject assetLoader = new GameObject();
             assetLoader.AddComponent<AssetLoaderObject>();
-            Object.DontDestroyOnLoad(assetLoader);
+            GameObject.DontDestroyOnLoad(assetLoader);
         }
 
         internal static void InitializeVersionLabel()
@@ -50,6 +56,30 @@ namespace Winch.Core
             GameObject term = new GameObject();
             term.AddComponent<Terminal>();
             UnityEngine.Object.DontDestroyOnLoad(term);
+        }
+
+        private static readonly HttpClient client = new HttpClient();
+        private static async void CheckForUpdate()
+        {
+            string latestPath = "https://github.com/Hacktix/Winch/releases/latest";
+            string content = await client.GetStringAsync(latestPath);
+
+            Regex titleRegex = new Regex("(?<=<title>)(.*)(?= · Hacktix)");
+            Match match = titleRegex.Match(content);
+
+            string latest = match.Value.Split(' ')[2];
+
+            string updateAvailableString;
+            if (VersionUtil.IsSameOrNewer(VersionUtil.GetVersion(), latest))
+            {
+                updateAvailableString = $"Latest version installed.";
+            }
+            else
+            {
+                updateAvailableString = $"Update {latest} available.";
+            }
+
+            GameManager.Instance.BuildInfo.BuildNumber += $"\n{updateAvailableString}";
         }
     }
 }
