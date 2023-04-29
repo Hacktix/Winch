748c3c8e128fac5efa5e62652a40460b6526c6ea
Hacktix <zecmad16@htlkaindorf.at>
2023-04-29 14:35:25 +0200

    add automatic Harmony patching

--- Winch/Core/WinchCore.cs
+++ Winch/Core/WinchCore.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using System;
 using Winch.Logging;
 using Winch.Util;
 
@@ -18,6 +19,24 @@ namespace Winch.Core
             var harmony = new Harmony("com.dredge.winch");
             Log.Debug("Created Harmony Instance 'com.dredge.winch'. Patching...");
             harmony.PatchAll();
+
+            foreach(ModAssembly modAssembly in ModAssemblyLoader.RegisteredAssemblies.Values)
+            {
+                try
+                {
+                    bool hasPatches = modAssembly.Metadata.ContainsKey("ApplyPatches") && (bool)modAssembly.Metadata["ApplyPatches"] == true;
+                    if (modAssembly.LoadedAssembly != null && hasPatches)
+                    {
+                        Log.Debug($"Patching from {modAssembly.LoadedAssembly.GetName().Name}...");
+                        harmony.PatchAll(modAssembly.LoadedAssembly);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Log.Error($"Failed to apply patches for {modAssembly.BasePath}: {ex}");
+                }
+            }
+
             Log.Debug("Harmony Patching complete.");
         }
     }
