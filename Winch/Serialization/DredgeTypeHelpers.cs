2008903a7741cbe162764dfbaebde7f66ec10ff3
SloopyNoSleep <131174641+SloopyNoSleep@users.noreply.github.com>
2023-05-02 10:51:01 +0200

    Add PoiUtil to help define and create POIs

--- Winch/Serialization/DredgeTypeHelpers.cs
+++ Winch/Serialization/DredgeTypeHelpers.cs
@@ -22,11 +22,22 @@ public static class DredgeTypeHelpers
     private static Color GetColorFromJsonDictionary(Dictionary<string, int> color)
     {
         int r = 0, g = 0, b = 0, a = 255;
-        if (color.TryGetValue("r", out var value)) r = value;
+       
         if (color.TryGetValue("g", out var value1)) g = value1;
         if (color.TryGetValue("b", out var value2)) b = value2;
         if (color.TryGetValue("a", out var value3)) a = value3;
         return new Color(r/255f, g/255f, b/255f, a/255f);
+    }
+
+    public static Vector3 ParseVector3(object inVal)
+    {
+        var position = JsonConvert.DeserializeObject<Dictionary<string, float>>(inVal.ToString()) ?? throw new InvalidOperationException("Unable to parse position.");
+        float x=0, y=0, z=0;
+        if (position.TryGetValue("x", out var value1)) x = value1;
+        if (position.TryGetValue("y", out var value2)) y = value2;
+        if (position.TryGetValue("z", out var value3)) z = value3;
+
+        return new Vector3(x, y, z);
     }
 
     public static List<Vector2Int> ParseDimensions(JArray dimensions)
---
+++ Winch/Util/PoiUtil.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using HarmonyLib;
+using UnityEngine;
+using Winch.Core;
+using Winch.Serialization;
+
+using Winch.Serialization.POI;
+using Winch.Serialization.POI.Conversation;
+using Winch.Serialization.POI.Harvest;
+
+namespace Winch.Util;
+
+internal static class PoiUtil
+{
+    private static Dictionary<Type, IDredgeTypeConverter> Converters = new()
+    {
+        //{ typeof(POI), new PoiConverter() },
+        //{ typeof(DockPOI), new DockPoiConverter() },
+        //{ typeof(ItemPOI), new ItemPoiConverter() },
+        //{ typeof(HarvestPOI), new HarvestPoiConverter() },
+        //{ typeof(HarvestPOIDataModel), new HarvestPoiDataModelConverter() },
+        //{ typeof(BaitHarvestPOI), new BaitPoiConverter() },
+        //{ typeof(PlacedHarvestPOI), new PlacedPoiConverter() },
+        //{ typeof(ConversationPOI), new ConversationPoiConverter() },
+        //{ typeof(AutoMovePOI), new AutoMovePoiConverter() },
+        //{ typeof(ConversationPOI), new ConversationPoiConverter() },
+        //{ typeof(ExplosivePOI), new ExplosivePoiConverter() },
+        { typeof(CustomHarvestPoi), new CustomHarvestPoiConverter()}
+    };
+    
+    public static List<CustomHarvestPoi> CustomHarvestPois = new();
+    public static Dictionary<string, IHarvestable> Harvestables = new();
+    public static Dictionary<string, GameObject> HarvestParticlePrefabs = new();
+    
+    public static void PopulateHarvestablesAndHarvestParticlePrefabs()
+    {
+        WinchCore.Log.Debug("Getting all harvestables and harvest particle prefabs");
+        // var baitPOIPrefab = Traverse.Create(banabil2).Field("baitPOIPrefab").GetValue() as GameObject;
+        var allHarvestPOIs = Traverse.Create(GameManager.Instance.HarvestPOIManager).Field("allHarvestPOIs").GetValue() as List<HarvestPOI>;
+        WinchCore.Log.Debug($"Got all POIs: {allHarvestPOIs.Count}");
+        foreach (var harvestPoi in allHarvestPOIs)
+        {
+            try
+            {
+                if (!Harvestables.ContainsKey(harvestPoi.Harvestable.GetId()))
+                    Harvestables.Add(harvestPoi.Harvestable.GetId(), harvestPoi.Harvestable);
+                var prefab = harvestPoi.Harvestable.GetParticlePrefab();
+                if (!HarvestParticlePrefabs.ContainsKey(prefab.name))
+                    HarvestParticlePrefabs.Add(prefab.name, prefab);
+            }
+            catch (Exception ex)
+            {
+                WinchCore.Log.Error($"Unable to add harvestable {harvestPoi.Harvestable.GetId()} to Harvestables and HarvestParticlePrefabs: {ex}");
+            }
+        }
+    }
+    
+    public static GameObject CreateGameObjectFromCustomHarvestPoi(CustomHarvestPoi customHarvestPoi)
+    {
+        WinchCore.Log.Debug($"Enumerating properties of customHarvestPoi {customHarvestPoi.id}: {customHarvestPoi}");
+        foreach(var descriptor in typeof(CustomHarvestPoi).GetFields())
+        {
+            string name = descriptor.Name;
+            object value = descriptor.GetValue(customHarvestPoi);
+            WinchCore.Log.Debug($"{name}={value}");
+        }
+        
+        GameObject customPoi = new GameObject();
+        customPoi.transform.SetParent(GameSceneInitializer.Instance.HarvestPoiContainer.transform);
+        customPoi.transform.position = customHarvestPoi.location;
+        customPoi.name = customHarvestPoi.id;
+        var harvestPoi = customPoi.AddComponent<HarvestPOI>();
+
+        var harvestPoiDataModel = new HarvestPOIDataModel();
+        harvestPoiDataModel.doesRestock = customHarvestPoi.doesRestock;
+        harvestPoiDataModel.startStock = customHarvestPoi.startStock;
+        harvestPoiDataModel.usesTimeSpecificStock = customHarvestPoi.useTimeSpecificStock;
+        
+        foreach (var item in customHarvestPoi.items)
+        {
+            if (ItemUtil.HarvestableItemDataDict.TryGetValue(item, out var value))
+                harvestPoiDataModel.items.Add((HarvestableItemData)value);
+        }
+        
+        foreach (var item in customHarvestPoi.nightItems)
+        {
+            if (ItemUtil.HarvestableItemDataDict.TryGetValue(item, out var value))
+                harvestPoiDataModel.nightItems.Add((HarvestableItemData)value);
+        }
+        
+        harvestPoiDataModel.id = customHarvestPoi.id;
+
+        harvestPoi.HarvestPOIData = harvestPoiDataModel;
+        harvestPoi.Harvestable = harvestPoiDataModel;
+        
+        GameObject harvestParticlePrefab = null;
+        if (HarvestParticlePrefabs.TryGetValue(customHarvestPoi.harvestableParticlePrefab, out var prefab))
+        {
+            harvestParticlePrefab = prefab;
+        }
+
+        Traverse.Create(harvestPoi).Field("harvestParticlePrefab").SetValue(harvestParticlePrefab);
+
+
+        // Default Harvest POI Sphere Collider
+        var sphereCollider = customPoi.AddComponent<SphereCollider>();
+        sphereCollider.radius = 2;
+        sphereCollider.enabled = true;
+        sphereCollider.contactOffset = 0.01f;
+        
+        Traverse.Create(harvestPoi).Field("poiCollider").SetValue(sphereCollider);
+        
+        // This needs to be added to the GameManager.Instance.CullingBrain
+        var cullable = customPoi.AddComponent<Cullable>();
+        GameManager.Instance.CullingBrain.AddCullable(cullable);
+
+        // No setup needed
+        customPoi.AddComponent<SimpleBuoyantObject>();
+
+        var allPOIs = Traverse.Create(GameManager.Instance.HarvestPOIManager).Field("allHarvestPOIs").GetValue() as List<HarvestPOI>;
+        allPOIs.Add(harvestPoi);
+
+        customPoi.layer = LayerMask.NameToLayer("POI");
+        return customPoi;
+    }
+    
+    internal static void AddCustomHarvestPoiFromMetadata(string metaPath)
+    {
+        var meta = UtilHelpers.ParseMeta(metaPath);
+        
+        if (meta == null)
+        {
+            WinchCore.Log.Error($"Meta file {metaPath} is empty");
+            return;
+        }
+
+        meta["id"] = Path.GetFileNameWithoutExtension(metaPath);
+        CustomHarvestPoi item = new();
+
+        foreach (var kvp in meta)
+        {
+            WinchCore.Log.Debug($"{kvp.Key} = {kvp.Value}");
+        }
+
+        if (UtilHelpers.PopulateObjectFromMeta(item, meta, Converters))
+        {
+            WinchCore.Log.Debug($"Adding custom harvest poi {item.id}");
+           CustomHarvestPois.Add(item);
+        }
+        else
+        {
+            WinchCore.Log.Error($"No converter found for type {typeof(CustomHarvestPoi)}");
+        }
+    }
+    
+}
