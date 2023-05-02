8325716c83470439949291fa7d7e0e7ab06656b1
SloopyNoSleep <131174641+SloopyNoSleep@users.noreply.github.com>
2023-05-02 17:46:25 +0200

    Add PlayerPatch changes

--- Winch/Patches/API/PlayerPatcher.cs
+++ Winch/Patches/API/PlayerPatcher.cs
@@ -1,6 +1,24 @@
-﻿namespace Winch.Patches.API;
+﻿using System;
+using HarmonyLib;
+using Winch.Core;
+using Winch.Util;
 
-public class PlayerPatcher
+namespace Winch.Patches.API;
+
+[HarmonyPatch(typeof(Player))]
+[HarmonyPatch("OnEnable")]
+public class PlayerPatch
 {
-    
+    public static void Postfix(Player __instance)
+    {
+        WinchCore.Log.Debug("Awake Prefix");
+        try
+        {
+            ItemUtil.PopulateItemData();
+        }
+        catch (Exception ex)
+        {
+            WinchCore.Log.Error($"Error in {nameof(PlayerPatch)}: exception {ex}");
+        }
+    }
 }
