93747cb52e2a534029283abc7ee767b9e94d11fe
bdlm-dev <74147859+bdlm-dev@users.noreply.github.com>
2023-05-05 18:54:40 +0100

    Rework config to allow subdirectories and custom config file names.

--- Winch/Config/ModConfig.cs
+++ Winch/Config/ModConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using Winch.Core;
 
@@ -10,7 +11,7 @@ namespace Winch.Config
         private static Dictionary<string, string> DefaultConfigs = new Dictionary<string, string>();
         private static Dictionary<string, ModConfig> Instances = new Dictionary<string, ModConfig>();
 
-        private ModConfig(string modName) : base(GetConfigPath(modName), GetDefaultConfig(modName))
+        private ModConfig(string modName, string fileName) : base(GetConfigPath(modName, fileName), GetDefaultConfig(modName))
         {
         }
 
@@ -18,27 +19,45 @@ namespace Winch.Config
         {
             if(!DefaultConfigs.ContainsKey(modName))
             {
-                WinchCore.Log.Error($"No 'DefaultConfig' attribute found in mod_meta.json for {modName}!");
-                throw new KeyNotFoundException($"No 'DefaultConfig' attribute found in mod_meta.json for {modName}!");
+                WinchCore.Log.Debug($"No default config found for {modName}");
+                DefaultConfigs.Add(modName, "{}");
             }
             return DefaultConfigs[modName];
         }
 
-        private static string GetConfigPath(string modName)
+        private static string GetConfigPath(string configLocation, string fileName="Config.json")
         {
-            return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Config", modName, "Config.json");
+            string basePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Config");
+            string output = Path.Combine(basePath, configLocation, fileName);
+            if (Directory.Exists(Path.Combine(basePath, configLocation)))
+                return output;
+            Directory.CreateDirectory(Path.Combine("Config", configLocation));
+            return output;
         }
 
-        private static ModConfig GetConfig(string modName)
+        private static ModConfig GetConfig(string modName, string fileName)
         {
             if (!Instances.ContainsKey(modName))
-                Instances.Add(modName, new ModConfig(modName));
+                Instances.Add(modName, new ModConfig(modName, fileName));
             return Instances[modName];
         }
 
-        public static T? GetProperty<T>(string modName, string key, T? defaultValue)
+        private static ModConfig GetConfig(string modName, string fileName, string subDirectory)
         {
-            return GetConfig(modName).GetProperty(key, defaultValue);
+            string _path = Path.Combine(modName, subDirectory);
+            if (!Instances.ContainsKey(_path))
+                Instances.Add(_path, new ModConfig(_path, fileName));
+            return Instances[_path];
+        }
+
+        public static T? GetProperty<T>(string modName, string key, T? defaultValue, string fileName="Config.json")
+        {
+            return GetConfig(modName, fileName).GetProperty(key, defaultValue);
+        }
+
+        public static T? GetProperty<T>(string modName, string key, T? defaultValue, string fileName="Config.json", string subDirectory="")
+        {
+            return GetConfig(modName, fileName, subDirectory).GetProperty(key, defaultValue);
         }
 
         public static void RegisterDefaultConfig(string modName, string config)
